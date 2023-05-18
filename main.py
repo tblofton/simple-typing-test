338d8a4857e04943ff6382bffba7c5599bad9703
tblofton <130260158+tblofton@users.noreply.github.com>
2023-05-18 20:51:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+String = 'The fox rocks socks that have red polka dots.'
+
+wordcount = len(String.split())
+
+print(String)
+
+while True:
+  t0=time.time()
+  inputtext=str(input('Enter the Sentence:'))
+  t1=time.time()
+  accuracy=len(set(inputtext.split()) &set(String.split()))
+  accuracy= accuracy/wordcount
+  timetaken = t1-t0
+  wpm= wordcount/timetaken
+  print('WPM:', wpm,'Accuracy:', accuracy, 'Time Taken:', timetaken)
